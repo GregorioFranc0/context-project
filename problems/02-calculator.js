7f13cf2caeb82da7263fcf0e6d0a3333a0074a56
gregorio <gregorio.franco1985@gmail.com>
2023-08-21 22:51:41 -0400

    phase 2

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,4 +1,20 @@
-// Your code here
+class Calculator {
+	constructor(total) {
+		this.total = 0;
+	}
+	add(num) {
+		return num + this.total;
+	}
+	subtract(num) {
+		return num - this.total;
+	}
+	divide(num) {
+		return num / this.total;
+	}
+	multiply(num) {
+		return num * this.total;
+	}
+}
 
 
 /*****************************************************************************/
